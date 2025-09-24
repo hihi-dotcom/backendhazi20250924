8ed3b5c35dd35be0eb2720a27f51311dfc02a5b4
Adam Czirjak <124435921+hihi-dotcom@users.noreply.github.com>
2025-09-24 21:19:03 +0200

    hazai feladat

--- src/controller.ts
+++ src/controller.ts
@@ -1,7 +1,14 @@
-export const valasz = (req:any, res: any) => {
+import Kutya from "./kutya";
+import { data } from "./data";
+const kutyus = new Kutya(data[0]);
+export const valasz = (_req:any, res: any) => {
     res.status(208).send("sodv");
 }
 
-export const valasz2 = (req1:any, res1: any) => {
+export const valasz2 = (_req1:any, res1: any) => {
     res1.status(208).send("sodv");
+}
+
+export const valasz3 = (_req2:any, res2: any) => {
+    res2.status(205).send(kutyus.renderTable("dogtable", data));
 }
--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,8 @@
 import {Router} from "express"
-import { valasz, valasz2 } from  "./controller"
+import { valasz, valasz2, valasz3 } from  "./controller"
 
 const router: Router = Router()
 router.get("/apik", valasz);
 router.get("/apik", valasz2);
-
+router.get("/kutyak", valasz3);
 export default router;
